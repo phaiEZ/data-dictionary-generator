556e0c19028d1bcc9dea8263457c804f71ec4116
phai <chayut@gmail.com>
2023-07-17 13:45:28 +0700

    edit download

--- src/main/java/com/example/dataDictionary/createDocx.java
+++ src/main/java/com/example/dataDictionary/createDocx.java
@@ -5,10 +5,18 @@ import java.io.IOException;
 
 public class createDocx {
     public static void createDocument() {
+
+        String[][] data = {
+                {"COLUMN", "TYPE", "NULL", "DEFAULT", "COMMENT"},
+                {"column2", "type2", "true", "default_value2", "Comment for column2"},
+                {"column3", "type3", "true", "default_value3", "Comment for column3"},
+                {"column4", "type4", "false", "null", "Comment for column4"}
+        };
+
         XWPFDocument document = new XWPFDocument();
 
         // Create a table with 2 rows and 3 columns
-        XWPFTable table = document.createTable(2, 3);
+        XWPFTable table = document.createTable(data.length, 5);
 
         // Set table width (optional)
         table.setWidth("100%");
@@ -20,15 +28,17 @@ public class createDocx {
         table.setInsideVBorder(XWPFTable.XWPFBorderType.SINGLE, 1, 1, "000000");
 
         // Set table cell content
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 3; col++) {
+
+        for (int row = 0; row < data.length; row++) {
+            for (int col = 0; col < 5; col++) {
                 XWPFTableCell cell = table.getRow(row).getCell(col);
-                cell.setText("Row " + (row + 1) + ", Column " + (col + 1));
+                cell.setText(data[row][col]);
             }
         }
+        String filePath = "/resources/templates/file.docx";
 
         // Save the document to a file
-        try (FileOutputStream out = new FileOutputStream("example.docx")) {
+        try (FileOutputStream out = new FileOutputStream(filePath)) {
             document.write(out);
             System.out.println("Document created successfully!");
         } catch (IOException e) {
