30c6094ea745fed57ae4b98d9c2c2604298bd4ae
phai <chayut@gmail.com>
2023-07-18 16:55:54 +0700

    download when created

--- src/main/java/com/example/dataDictionary/createDocx.java
+++ src/main/java/com/example/dataDictionary/createDocx.java
@@ -1,55 +1,46 @@
 package com.example.dataDictionary;
 
 import org.apache.poi.xwpf.usermodel.*;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.File;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.ByteArrayOutputStream;
 
 public class createDocx {
-    public static void createDocument(String[][][] tableDataList) {
-
+    public static ResponseEntity<ByteArrayResource> createDocument(String[][][] tableDataList) {
         try (XWPFDocument document = new XWPFDocument()) {
-
             for (String[][] data : tableDataList) {
-                String tableName = data[0][0];
-
-                XWPFParagraph tableNameParagraph = document.createParagraph();
-                XWPFRun tableNameRun = tableNameParagraph.createRun();
-                tableNameRun.setText(tableName);
-                tableNameParagraph.setSpacingAfter(200);
-
-                XWPFTable table = document.createTable(data.length - 1, data[1].length);
-
-                table.setWidth("100%");
-
-                table.setTopBorder(XWPFTable.XWPFBorderType.SINGLE, 1, 1, "000000");
-                table.setBottomBorder(XWPFTable.XWPFBorderType.SINGLE, 1, 1, "000000");
-                table.setInsideHBorder(XWPFTable.XWPFBorderType.SINGLE, 1, 1, "000000");
-                table.setInsideVBorder(XWPFTable.XWPFBorderType.SINGLE, 1, 1, "000000");
-
-                for (int row = 1; row < data.length; row++) {
-                    for (int col = 0; col < data[row].length; col++) {
-                        XWPFTableCell cell = table.getRow(row - 1).getCell(col);
-                        cell.setText(data[row][col]);
-                    }
-                }
+                // ... your existing code to create the tables
 
                 XWPFParagraph space = document.createParagraph();
                 space.setSpacingAfter(500);
             }
 
-            String filePath = "src/main/resources/templates/file.docx";
-            File file = new File(filePath);
-            file.delete();
-
-            try (FileOutputStream out = new FileOutputStream(filePath)) {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
                 document.write(out);
                 System.out.println("Document created successfully!");
+
+                byte[] fileContent = out.toByteArray();
+                ByteArrayResource resource = new ByteArrayResource(fileContent);
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                headers.setContentDispositionFormData("attachment", "file.docx");
+                headers.setContentLength(fileContent.length);
+
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(resource);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return ResponseEntity.notFound().build();
     }
 }
